d7a4759b89ac31d5169c67fc3f19e94394cb83dd
guypierce <14934009+guypierce@users.noreply.github.com>
2018-06-09 21:25:48 -0600

    Create converter.js

---
+++ converter.js
@@ -0,0 +1,5 @@
+let userAge = 50 //Users age
+const earlyYears = 2 * 10.5; //First 2 years of users age * 10.0
+const laterYears = (userAge - 2) * 4; //Rest of users age * 4
+const myAgeInDogYears = `My age is ${earlyYears + laterYears} in Dog Years!`;
+console.log(myAgeInDogYears);
