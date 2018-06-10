85dd3762e8c8d6b7d92a63d8e89254b086ca162f
guypierce <14934009+guypierce@users.noreply.github.com>
2018-06-09 21:27:08 -0600

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,3 +1,4 @@
+// (TODO) Add user input functionality
 let userAge = 50 //Users age
 const earlyYears = 2 * 10.5; //First 2 years of users age * 10.0
 const laterYears = (userAge - 2) * 4; //Rest of users age * 4
